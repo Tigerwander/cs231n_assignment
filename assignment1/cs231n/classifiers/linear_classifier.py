fix softmax bug

assignment1/cs231n/classifiers/linear_classifier.py
@@ -73,12 +73,13 @@ class LinearClassifier(object):
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
       #pass
-      self.W = self.W -learning_rate * grad
+      self.W = self.W - learning_rate * grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
 
-      if verbose and it % 100 == 0:
+      #if verbose and it % 100 == 0:
+      if verbose:
         pred = self.predict(X_batch)
         accuracy = np.mean(pred == y_batch)
         print('iteration %d / %d: loss %f, accuracy %f' % (it, num_iters, loss, accuracy))
@@ -142,5 +143,6 @@ class Softmax(LinearClassifier):
   """ A subclass that uses the Softmax + Cross-entropy loss function """
 
   def loss(self, X_batch, y_batch, reg):
-    return softmax_loss_vectorized(self.W, X_batch, y_batch, reg)
+    #return softmax_loss_vectorized(self.W, X_batch, y_batch, reg)
+    return softmax_loss_naive(self.W, X_batch, y_batch, reg)
 

assignment1/cs231n/classifiers/softmax.py
@@ -35,21 +35,21 @@ def softmax_loss_naive(W, X, y, reg):
   num_classes = W.shape[1]
 
   for ii in xrange(num_training):
-      scores = X[ii, :].dot(W)
-      true_score = scores[y[ii]]
+      scores = X[ii].dot(W)
       max_score = np.max(scores)
       scores -= max_score
-      exp_scores = np.exp(scores)
-      sum_exp = np.sum(exp_scores)
-      exp_scores /= sum_exp
-      loss += -np.log(exp_scores[y[ii]])
+      sum_exp = np.sum(np.exp(scores))
+      exp_scores = lambda k: np.exp(scores[k]) / sum_exp
+      #exp_scores /= sum_exp
+      loss += -np.log(exp_scores(y[ii]))
 
       for kk in range(num_classes):
-          dw[:, kk] += (exp_scores[kk] - (kk == y[ii])) * X[ii, :]
+          dW[:, kk] += (exp_scores(kk) - (kk == y[ii])) * X[ii]
 
   loss /= num_training
   loss += 0.5 * reg * np.sum(W * W)
-  dW /= num_train
+
+  dW /= num_training
   dW += reg * W
 
   #############################################################################
@@ -83,7 +83,7 @@ def softmax_loss_vectorized(W, X, y, reg):
   scores = np.exp(scores)
   scores /= np.sum(scores, axis=1, keepdims=True)
 
-  loss = -np.sum(np.log(scores[np.arange(num_training, y)]))
+  loss = np.sum(-np.log(scores[np.arange(num_training), y]))
   loss /= num_training
   loss += 0.5 * reg * np.sum(W * W)
 

assignment1/test_classfier.py
@@ -1,5 +1,6 @@
 from cs231n.classifiers import KNearestNeighbor
 from cs231n.classifiers import LinearSVM
+from cs231n.classifiers import Softmax
 from cs231n.data_utils import get_CIFAR10_data
 import numpy as np
 
@@ -29,10 +30,24 @@ def test_svm(data):
     accuracy = np.mean(predict == data['y_test'])
     print('svm predict accuracy {}'.format(accuracy))
 
+def test_softmax(data):
+    softmax = Softmax()
+    num_training = data['X_train'].shape[0]
+    train = data['X_train'].reshape(num_training, 32 * 32 * 3)
+    print('training...')
+    softmax.train(train, data['y_train'], num_iters=1000, verbose=True, reg=1e-3)
+
+    num_test = data['X_test'].shape[0]
+    test = data['X_test'].reshape(num_test, 32 * 32 * 3)
+    predict = softmax.predict(test)
+    accuracy = np.mean(predict == data['y_test'])
+    print('softmax predict accuracy {}'.format(accuracy))
+
 
 
 if __name__ == '__main__':
     data = get_CIFAR10_data(num_training=5000)
 
-    test_knn(data)
-    test_svm(data)
+    #test_knn(data)
+    #test_svm(data)
+    test_softmax(data)